8ada5bc1a0194d397ab3bba22a3c74db8f7f8f98
Ekta Sahu <83973342+Ektasa@users.noreply.github.com>
2026-03-02 23:52:42 +0530

    API gateway

--- APiGateway/src/main/java/com/api/APiGateway/APiGatewayApplication.java
+++ APiGateway/src/main/java/com/api/APiGateway/APiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.hotel.service.HotelService;
+package com.api.APiGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class HotelServiceApplication {
+public class APiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HotelServiceApplication.class, args);
+		SpringApplication.run(APiGatewayApplication.class, args);
 	}
 
 }
---
+++ APiGateway/src/test/java/com/api/APiGateway/APiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.APiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
